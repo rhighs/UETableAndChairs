f30e23dda1f05139fc8da638e9ea0477d42f558b
Roberto Montalti <roberto.montalti00@gmail.com>
2022-05-05 22:46:18 +0200

    Most basic version of a procedurally generated chair

--- Source/TableAndChairs/BlockyChair.cpp
+++ Source/TableAndChairs/BlockyChair.cpp
@@ -4,7 +4,9 @@ Mesh BlockyChair(FVector size)
 {
 	Mesh chairMesh;
 
-	const float restHeightRatio = 0.1f;
+	const float chairRadius = size.X / 2;
+
+	const float restHeightRatio = 0.07f;
 	const float backHeightRatio = 0.5f;
 	const float legsHeightRatio = 0.4f;
 
@@ -16,17 +18,23 @@ Mesh BlockyChair(FVector size)
 	const float restWidthRatio = 1.0f;
 	const float legsWidthRatio = 0.2f;
 
-	const FVector restSize(size.X * restWidthRatio, size.Y * restLengthRatio, size.Z * restHeightRatio);
+	const FVector restSize(size.X * restWidthRatio, size.Y, size.Z * restHeightRatio);
 	const FVector backSize(size.X * backWidthRatio, size.Y * backLengthRatio, size.Z * backHeightRatio);
 	const FVector legsSize(size.X * legsWidthRatio, size.Y * legsLengthRatio, size.Z * legsHeightRatio);
 
-	CubeMesh(chairMesh, restSize);
-	CubeMesh(chairMesh, backSize);
+	const FVector restPosition(0.0f,							     0.0f, 		 legsSize.Z + restSize.Z / 2);
+	const FVector backPosition(0.0f,		-chairRadius + backSize.Y / 2, 	 restPosition.Z + backSize.Z / 2 + restSize.Z / 2);
 
-	for (size_t i = 0; i < 4; i++)
-	{
-		CubeMesh(chairMesh, legsSize, FVector::ZeroVector /*<- Should be some offset*/);
-	}
+	CubeMesh(chairMesh, restSize, restPosition);
+	CubeMesh(chairMesh, backSize, backPosition);
+	
+	float legRadius = legsSize.X / 2;
+
+	// Please don't touch this
+	CubeMesh(chairMesh, legsSize, FVector(legRadius - chairRadius, legRadius - chairRadius, legsSize.Z / 2));
+	CubeMesh(chairMesh, legsSize, FVector(- legRadius + chairRadius, legRadius - chairRadius, legsSize.Z / 2));
+	CubeMesh(chairMesh, legsSize, FVector(legRadius - chairRadius, - legRadius + chairRadius, legsSize.Z / 2));
+	CubeMesh(chairMesh, legsSize, FVector(- legRadius + chairRadius, - legRadius + chairRadius, legsSize.Z / 2));
 
 	return chairMesh;
 }
